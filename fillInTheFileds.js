158024dbbbf0e4c23679f5402abdf4609f21be3d
Timur Aibulatov <t.aibulatov@gmail.com>
2020-02-25 23:36:24 +0400

    Create fillInTheFileds.js

---
+++ fillInTheFileds.js
@@ -0,0 +1,103 @@
+javascript: (function() {
+
+    var name = generateRandomString(10);
+    var surname = generateRandomString(10);
+    var phoneNumber = generateRandomPhoneNumber();
+    var email = getDateTime() + "@test.com";
+    var message = "Hi! I would like to start working on the project on " + getStartDate();
+    var dynamicSelectId = document.getElementsByTagName("select")[1].id;
+    var selectId = document.getElementById(dynamicSelectId);
+
+    var FORM_LOCATOR = "//div[@id='something-else']//form[contains(@id, 'hsForm_')]";
+    var NAME_INPUT_LOCATOR = FORM_LOCATOR + "//input[contains(@id,'firstname-')]";
+    var SURNAME_INPUT_LOCATOR = FORM_LOCATOR + "//input[contains(@id,'lastname-')]";
+    var EMAIL_INPUT_LOCATOR = FORM_LOCATOR + "//input[contains(@id,'email-')]";
+    var PHONE_INPUT_LOCATOR = FORM_LOCATOR + "//input[contains(@id,'phone-')]";
+    var MESSAGE_INPUT_LOCATOR = FORM_LOCATOR + "//textarea[contains(@id,'notes__')]";
+
+    function typeValueInTheField(elem, val) {
+        var input = getElementByXpath(elem);
+        input.value = val;
+        input.dispatchEvent(new Event('input'));
+    }
+
+    function getElementByXpath(path) {
+        return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    }
+
+    function getDateTime() {
+        var now = new Date();
+        var day = now.getDate();
+        var month = now.getMonth() + 1;
+        var year = now.getFullYear();
+        var hour = now.getHours();
+        var minute = now.getMinutes();
+        var second = now.getSeconds();
+        var millis = now.getMilliseconds();
+        if (day.toString().length == 1) {
+            var day = '0' + day;
+        }
+        if (month.toString().length == 1) {
+            var month = '0' + month;
+        }
+        if (hour.toString().length == 1) {
+            var hour = '0' + hour;
+        }
+        if (minute.toString().length == 1) {
+            var minute = '0' + minute;
+        }
+        if (second.toString().length == 1) {
+            var second = '0' + second;
+        }
+        var dateTime = day + month + year + hour + minute + second + millis;
+        return dateTime;
+    }
+
+    function getStartDate() {
+        var now = new Date();
+        now.setDate(now.getDate() + 3); //+3 days from the current date
+
+        var day = now.getDate();
+        var month = now.getMonth() + 1;
+        var year = now.getFullYear();
+
+        var startDate = day + "/" + month + "/" + year;
+        return startDate;
+    }
+
+    function generateRandomString(length) {
+        var text = "";
+        var possible = "abcdefghijklmnopqrstuvwxyz";
+
+        for (var i = 0; i < length; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+
+        return text[0].toUpperCase() + text.slice(1);
+    }
+
+    function generateRandomPhoneNumber() {
+        var length = 10;
+        var phone = "";
+        var possible = "0123456789";
+
+        for (var i = 0; i < length; i++)
+            phone += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return "+7" + phone;
+    }
+
+    function selectItemByValue(elem, value) {
+        for (var i = 0; i < elem.options.length; i++) {
+            if (elem.options[i].value == value)
+                elem.selectedIndex = i;
+        }
+    }
+
+    selectItemByValue(selectId, "A career with Omada");
+    typeValueInTheField(NAME_INPUT_LOCATOR, name);
+    typeValueInTheField(SURNAME_INPUT_LOCATOR, surname);
+    typeValueInTheField(EMAIL_INPUT_LOCATOR, email);
+    typeValueInTheField(PHONE_INPUT_LOCATOR, phoneNumber);
+    typeValueInTheField(MESSAGE_INPUT_LOCATOR, message);
+})();
